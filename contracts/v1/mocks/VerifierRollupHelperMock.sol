dfec4561c34f5b3948e5deab6cb41ccbeb26539c
Laia Soler <laia@polygon.technology>
2025-05-23 16:31:12 +0200

    v2 --> contracts, contracts --> v1

---
+++ contracts/v1/mocks/VerifierRollupHelperMock.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity 0.8.28;
+
+import "../../interfaces/IVerifierRollup.sol";
+import "../../interfaces/ISP1Verifier.sol";
+
+contract VerifierRollupHelperMock is IVerifierRollup, ISP1Verifier {
+    function verifyProof(
+        bytes32[24] calldata proof,
+        uint256[1] memory pubSignals
+    ) public pure override returns (bool) {
+        return true;
+    }
+
+    // SP1 interface
+    function verifyProof(
+        bytes32 programVKey,
+        bytes calldata publicValues,
+        bytes calldata proofBytes
+    ) public pure {}
+}
