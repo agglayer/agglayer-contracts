c2880d43e5e1388202b7759fc9e9b3dc149d48fe
Laia Soler <laia@polygon.technology>
2025-05-26 12:26:20 +0200

    previousVersions -> contracts

--- contracts/previousVersions/pessimistic/PolygonRollupBaseEtrogPessimistic.sol
+++ contracts/previousVersions/pessimistic/PolygonRollupBaseEtrogPessimistic.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable4/token/ERC20/utils/SafeERC20Upgradeable.sol";
-import "../../../interfaces/IPolygonZkEVMGlobalExitRootV2.sol";
-import "../../../interfaces/IPolygonZkEVMErrors.sol";
-import "../../../interfaces/IPolygonZkEVMEtrogErrors.sol";
+import "../../interfaces/IPolygonZkEVMGlobalExitRootV2.sol";
+import "../../interfaces/IPolygonZkEVMErrors.sol";
+import "../../interfaces/IPolygonZkEVMEtrogErrors.sol";
 import "./PolygonRollupManagerPessimistic.sol";
-import "../../../interfaces/IPolygonRollupBase.sol";
-import "../../../interfaces/IPolygonZkEVMBridgeV2.sol";
+import "../../interfaces/IPolygonRollupBase.sol";
+import "../../interfaces/IPolygonZkEVMBridgeV2.sol";
 import "@openzeppelin/contracts-upgradeable4/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
-import "../../../lib/PolygonConstantsBase.sol";
+import "../../lib/PolygonConstantsBase.sol";
 import "./PolygonConsensusBasePessimistic.sol";
 
 /**
