JoshMa-mq/zkevm-contracts

update tools ts, prettier & eslint

src/utils-aggchain-ECDSA.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable no-restricted-syntax */
-const ethers = require('ethers');
+import ethers from 'ethers';
 
 /// ////////////////////////////////////
 /// // Constants for Aggchain ECDSA ////
 /// ////////////////////////////////////
 
 // aggchain type selector for ECDSA
-const AGGCHAIN_TYPE_ECDSA = '0x0000';
+export const AGGCHAIN_TYPE_ECDSA = '0x0000';
 
 /// ////////////////////////////////////
 /// // Functions for Aggchain ECDSA ////
@@ -26,7 +26,7 @@ const AGGCHAIN_TYPE_ECDSA = '0x0000';
  * @param {String} networkName L2 network name
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeInitializeBytesAggchainECDSAv0(
+export function encodeInitializeBytesAggchainECDSAv0(
     useDefaultGateway,
     ownedAggchainVKey,
     aggchainVKeyVersion,
@@ -61,7 +61,7 @@ function encodeInitializeBytesAggchainECDSAv0(
  * @param {String} vKeyManager vkey manager address
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeInitializeBytesAggchainECDSAv1(
+export function encodeInitializeBytesAggchainECDSAv1(
     useDefaultGateway,
     ownedAggchainVKey,
     aggchainVKeyVersion,
@@ -69,12 +69,7 @@ function encodeInitializeBytesAggchainECDSAv1(
 ) {
     return ethers.AbiCoder.defaultAbiCoder().encode(
         ['bool', 'bytes32', 'bytes2', 'address'],
-        [
-            useDefaultGateway,
-            ownedAggchainVKey,
-            aggchainVKeyVersion,
-            vKeyManager,
-        ],
+        [useDefaultGateway, ownedAggchainVKey, aggchainVKeyVersion, vKeyManager],
     );
 }
 
@@ -84,7 +79,7 @@ function encodeInitializeBytesAggchainECDSAv1(
  * @param {String} newStateRoot new state root
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeAggchainDataECDSA(aggchainVKeyVersion, newStateRoot) {
+export function encodeAggchainDataECDSA(aggchainVKeyVersion, newStateRoot) {
     return ethers.AbiCoder.defaultAbiCoder().encode(['bytes2', 'bytes32'], [aggchainVKeyVersion, newStateRoot]);
 }
 
@@ -93,14 +88,6 @@ function encodeAggchainDataECDSA(aggchainVKeyVersion, newStateRoot) {
  * @param {String} trustedSequencer Trusted sequencer address
  * @returns {String} hash of encoded value in hexadecimal string (32 bytes)
  */
-function computeHashAggchainParamsECDSA(trustedSequencer) {
+export function computeHashAggchainParamsECDSA(trustedSequencer) {
     return ethers.solidityPackedKeccak256(['address'], [trustedSequencer]);
 }
-
-module.exports = {
-    AGGCHAIN_TYPE_ECDSA,
-    encodeInitializeBytesAggchainECDSAv0,
-    encodeInitializeBytesAggchainECDSAv1,
-    encodeAggchainDataECDSA,
-    computeHashAggchainParamsECDSA,
-};

src/utils-aggchain-FEP.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable no-restricted-syntax */
-const ethers = require('ethers');
+import ethers from 'ethers';
 
 /// //////////////////////////////////
 /// // Constants for Aggchain FEP ////
 /// //////////////////////////////////
 
 // aggchain type selector for FEP
-const AGGCHAIN_TYPE_FEP = '0x0001';
+export const AGGCHAIN_TYPE_FEP = '0x0001';
 
 /// //////////////////////////////////
 /// // Functions for Aggchain FEP ////
@@ -27,7 +27,7 @@ const AGGCHAIN_TYPE_FEP = '0x0001';
  * @param {String} networkName network name
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeInitializeBytesAggchainFEPv0(
+export function encodeInitializeBytesAggchainFEPv0(
     initParams,
     useDefaultGateway,
     initOwnedAggchainVKey,
@@ -75,7 +75,7 @@ function encodeInitializeBytesAggchainFEPv0(
  * @param {String} initAggchainVKeyVersion initial aggchain Vkey version
  * @param {String} vKeyManager vkey manager address
  */
-function encodeInitializeBytesAggchainFEPv1(
+export function encodeInitializeBytesAggchainFEPv1(
     initParams,
     useDefaultGateway,
     initOwnedAggchainVKey,
@@ -90,13 +90,7 @@ function encodeInitializeBytesAggchainFEPv1(
             'bytes2',
             'address',
         ],
-        [
-            Object.values(initParams),
-            useDefaultGateway,
-            initOwnedAggchainVKey,
-            initAggchainVKeyVersion,
-            vKeyManager,
-        ],
+        [Object.values(initParams), useDefaultGateway, initOwnedAggchainVKey, initAggchainVKeyVersion, vKeyManager],
     );
 }
 
@@ -107,7 +101,7 @@ function encodeInitializeBytesAggchainFEPv1(
  * @param {Number} l2BlockNumber L2 block number
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeAggchainDataFEP(aggchainVKeyVersion, outputRoot, l2BlockNumber) {
+export function encodeAggchainDataFEP(aggchainVKeyVersion, outputRoot, l2BlockNumber) {
     return ethers.AbiCoder.defaultAbiCoder().encode(
         ['bytes2', 'bytes32', 'uint256'],
         [aggchainVKeyVersion, outputRoot, l2BlockNumber],
@@ -124,7 +118,7 @@ function encodeAggchainDataFEP(aggchainVKeyVersion, outputRoot, l2BlockNumber) {
  * @param {String} trustedSequencer trusted sequencer address
  * @returns aggchain param hash
  */
-function computeHashAggchainParamsFEP(
+export function computeHashAggchainParamsFEP(
     oldOutputRoot,
     newOutputRoot,
     l2BlockNumber,
@@ -138,11 +132,3 @@ function computeHashAggchainParamsFEP(
         [oldOutputRoot, newOutputRoot, l2BlockNumber, rollupConfigHash, optimisticMode, trustedSequencer],
     );
 }
-
-module.exports = {
-    AGGCHAIN_TYPE_FEP,
-    encodeInitializeBytesAggchainFEPv0,
-    encodeInitializeBytesAggchainFEPv1,
-    encodeAggchainDataFEP,
-    computeHashAggchainParamsFEP,
-};

agglayer/agglayer-contracts

src/utils-aggchain-ECDSA.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable no-restricted-syntax */
-const ethers = require('ethers');
+import ethers from 'ethers';
 
 /// ////////////////////////////////////
 /// // Constants for Aggchain ECDSA ////
 /// ////////////////////////////////////
 
 // aggchain type selector for ECDSA
-const AGGCHAIN_TYPE_ECDSA = '0x0000';
+export const AGGCHAIN_TYPE_ECDSA = '0x0000';
 
 /// ////////////////////////////////////
 /// // Functions for Aggchain ECDSA ////
@@ -26,7 +26,7 @@ const AGGCHAIN_TYPE_ECDSA = '0x0000';
  * @param {String} networkName L2 network name
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeInitializeBytesAggchainECDSAv0(
+export function encodeInitializeBytesAggchainECDSAv0(
     useDefaultGateway,
     ownedAggchainVKey,
     aggchainVKeySelector,
@@ -61,7 +61,7 @@ function encodeInitializeBytesAggchainECDSAv0(
  * @param {String} vKeyManager vkey manager address
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeInitializeBytesAggchainECDSAv1(
+export function encodeInitializeBytesAggchainECDSAv1(
     useDefaultGateway,
     ownedAggchainVKey,
     aggchainVKeySelector,
@@ -84,7 +84,7 @@ function encodeInitializeBytesAggchainECDSAv1(
  * @param {String} newStateRoot new state root
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeAggchainDataECDSA(aggchainVKeySelector, newStateRoot) {
+export function encodeAggchainDataECDSA(aggchainVKeySelector, newStateRoot) {
     return ethers.AbiCoder.defaultAbiCoder().encode(['bytes4', 'bytes32'], [aggchainVKeySelector, newStateRoot]);
 }
 
@@ -93,14 +93,6 @@ function encodeAggchainDataECDSA(aggchainVKeySelector, newStateRoot) {
  * @param {String} trustedSequencer Trusted sequencer address
  * @returns {String} hash of encoded value in hexadecimal string (32 bytes)
  */
-function computeHashAggchainParamsECDSA(trustedSequencer) {
+export function computeHashAggchainParamsECDSA(trustedSequencer) {
     return ethers.solidityPackedKeccak256(['address'], [trustedSequencer]);
 }
-
-module.exports = {
-    AGGCHAIN_TYPE_ECDSA,
-    encodeInitializeBytesAggchainECDSAv0,
-    encodeInitializeBytesAggchainECDSAv1,
-    encodeAggchainDataECDSA,
-    computeHashAggchainParamsECDSA,
-};

src/utils-aggchain-FEP.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable no-restricted-syntax */
-const ethers = require('ethers');
+import ethers from 'ethers';
 
 /// //////////////////////////////////
 /// // Constants for Aggchain FEP ////
 /// //////////////////////////////////
 
 // aggchain type selector for FEP
-const AGGCHAIN_TYPE_FEP = '0x0001';
+export const AGGCHAIN_TYPE_FEP = '0x0001';
 
 /// //////////////////////////////////
 /// // Functions for Aggchain FEP ////
@@ -18,7 +18,7 @@ const AGGCHAIN_TYPE_FEP = '0x0001';
  * @param {Object} initParams initialization parameters
  * @returns {Object} sorted initParams object
  */
-function sortInitParamsAggchainFEP(initParams) {
+export function sortInitParamsAggchainFEP(initParams) {
     const initParamsOrder = [
         'l2BlockTime',
         'rollupConfigHash',
@@ -55,7 +55,7 @@ function sortInitParamsAggchainFEP(initParams) {
  * @param {String} networkName network name
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeInitializeBytesAggchainFEPv0(
+export function encodeInitializeBytesAggchainFEPv0(
     initParams,
     useDefaultGateway,
     initOwnedAggchainVKey,
@@ -105,7 +105,7 @@ function encodeInitializeBytesAggchainFEPv0(
  * @param {String} initAggchainVKeySelector initial aggchain Vkey version
  * @param {String} vKeyManager vkey manager address
  */
-function encodeInitializeBytesAggchainFEPv1(
+export function encodeInitializeBytesAggchainFEPv1(
     initParams,
     useDefaultGateway,
     initOwnedAggchainVKey,
@@ -137,7 +137,7 @@ function encodeInitializeBytesAggchainFEPv1(
  * @param {Number} l2BlockNumber L2 block number
  * @returns {String} encoded value in hexadecimal string
  */
-function encodeAggchainDataFEP(aggchainVKeySelector, outputRoot, l2BlockNumber) {
+export function encodeAggchainDataFEP(aggchainVKeySelector, outputRoot, l2BlockNumber) {
     return ethers.AbiCoder.defaultAbiCoder().encode(
         ['bytes4', 'bytes32', 'uint256'],
         [aggchainVKeySelector, outputRoot, l2BlockNumber],
@@ -156,7 +156,7 @@ function encodeAggchainDataFEP(aggchainVKeySelector, outputRoot, l2BlockNumber) 
  * @param {String} aggregationVkey aggregationVkey
  * @returns aggchain param hash
  */
-function computeHashAggchainParamsFEP(
+export function computeHashAggchainParamsFEP(
     oldOutputRoot,
     newOutputRoot,
     l2BlockNumber,
@@ -188,12 +188,3 @@ function computeHashAggchainParamsFEP(
         ],
     );
 }
-
-module.exports = {
-    AGGCHAIN_TYPE_FEP,
-    encodeInitializeBytesAggchainFEPv0,
-    encodeInitializeBytesAggchainFEPv1,
-    encodeAggchainDataFEP,
-    computeHashAggchainParamsFEP,
-    sortInitParamsAggchainFEP,
-};